8e24565ef62cf8d1332a4d5afbaba7b93c2a0765
Ivan-Khudiakov <khudiakov1990@gmail.com>
2021-05-24 20:44:32 +0500

    + useEffect in ProfileInfoWithHooks

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, useState} from "react";
+import React, {ChangeEvent, useEffect, useState} from "react";
 
 type PropsType = {
     status: string
@@ -9,19 +9,22 @@ export const ProfileStatusWithHooks = (props: PropsType) => {
     let [editMode, setEditMode] = useState (false)
     let [status, setStatus] = useState(props.status)
 
+    useEffect(() => {
+        setStatus(props.status)
+    },[props.status])
+
     const activateEditMode = () => {
         setEditMode(true)
     }
-
     const deactivateEditMode = () => {
         setEditMode(false)
         //данный запрос необходи для сохранения нового статуса после деактивации
         props.updateStatus(status)
     }
-
     const onStatusChange = (e: ChangeEvent<HTMLInputElement>) => {
         setStatus(e.currentTarget.value)
     }
+
         return (
             <div>
                 {!editMode &&
